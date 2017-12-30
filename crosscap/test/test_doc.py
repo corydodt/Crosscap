06b06f088e061687f196fe83ce2e88a6c62b37f3
Cory Dodt <corydodt@gmail.com>
2017-12-29 22:45:41 -0800

    better test

--- crosscap/test/test_doc.py
+++ crosscap/test/test_doc.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """
 Tests of the doc class
 """
@@ -15,3 +16,37 @@ def test_doc():
         """
 
     assert doc.doc(aFunction) == "Hello"
+
+
+def test_documentation():
+    """
+    Do I find the doc on an object?
+
+    Do I extract first the first line?
+
+    Do I decode if asked to?
+    """
+    class Cls(object):
+        """
+        I have a bytestr docstring
+
+        It is 2 lines
+        """
+
+    class Unicls(object):
+        u"""
+        I have a unicode docstring
+        """
+
+    class StrClsWithUTF8(object):
+        """
+        I have a 😼💫 docstring
+        """
+
+    assert doc.doc(Cls) == "I have a bytestr docstring"
+
+    assert doc.Documentation.fromObject(Cls).full == "I have a bytestr docstring\n\nIt is 2 lines"
+    assert isinstance(doc.Documentation.fromObject(Unicls).first, unicode)
+    strDoc = doc.Documentation.fromObject(StrClsWithUTF8, decode=True)
+    assert strDoc.first == u'I have a 😼💫 docstring'
+    assert isinstance(strDoc.first, unicode)
