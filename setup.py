4505c21d1d19203aac54a4f8794a5d6d43d4fd08
Cory Dodt <121705+corydodt@users.noreply.github.com>
2019-07-23 12:13:17 -0700

    #20 specify less

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 _version = {}
 exec(open('crosscap/_version.py').read(), _version)
 
-setup(
+cfg = dict(
     name = 'Crosscap',
     packages=['crosscap/' + x for x in find_packages('crosscap')],
     version = _version['__version__'],
@@ -20,18 +20,29 @@ setup(
         "License :: OSI Approved :: MIT License"
     ],
     scripts = ['bin/urltool'],
+    extras_require={
+        # tests
+        'dev': [
+            "pytest-cov",
+            "pytest-twisted",
+            "pytest",
+            'mock',
+            'pyflakes',
+            'pytest-flakes',
+            'pytz',
+            'tox',
+        ],
+    },
     install_requires=cleandoc('''
         click>=6.7
         ftfy==4.4.3
         future>=0.16.0
         pyjwt>=1.7.1
-        pytest>=3.1.0
-        pytest-cov>=2.5.1
-        pytest-flakes>=2.0.0
-        pytest-twisted>=1.5
         pyyaml>=3.12,<4.0
         twisted>=17.1.0
         werkzeug>=0.13
         wrapt>=1.11.1
         ''').split()
 )
+
+setup(**cfg)
